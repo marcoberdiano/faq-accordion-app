3ea9582e1bcb786f80c346c2f00748a59a376c90
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2022-05-31 01:36:38 +0100

    fix the box position

--- src/components/FaqContainerLeft.jsx
+++ src/components/FaqContainerLeft.jsx
@@ -8,7 +8,11 @@ const FaqContainerLeft = () => {
             <div className='FaqContainerLeft'>
                  <div className='img-container'>
                     <img className='woman-img-desk' src={illustrationWomanD} alt='woman-online'/>
+                    
+                    <div className='box'>
                     <img className='img-box' src={illustrationBox} alt='box'/>
+                    </div>
+                    
                     <img className='woman-img-mobile' src={illustrationWomanM} alt='woman-online'/>
                 </div>
             </div>
